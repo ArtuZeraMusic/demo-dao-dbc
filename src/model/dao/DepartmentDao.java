23b7592f99ace612f36652594a9614e12ff4595b
ArtuZeraMusic <artur.silvestre88@gmail.com>
2022-12-27 18:48:47 -0300

    Continuando com SellerDaoJDBC e DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	//Operação que insere, atualiza e deleta no Banco de Dados o objeto obj parametrizado
+	void insert (Department obj);
+	void update (Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
 }
